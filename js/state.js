6b7cff670b82344eb80cb5a46874c24b62dfb282
Matias1661 <118617013+Matias1661@users.noreply.github.com>
2026-07-14 12:41:11 +0200

    state: agregar nominas al estado global

--- js/state.js
+++ js/state.js
@@ -3,6 +3,7 @@
 window.FINANCE_STATE = {
   raw: [],
   inversiones: { capital: [], rendimiento: [] },
+  nominas: [],
   excludedCategories: ['Guille', 'Inversion', 'Talho Argentino'],
   // Categorías donde un ingreso puede ser reembolso de un gasto
   reimbursableCategories: ['Viajes', 'Club', 'Combustible', 'Comer afuera', 'Salidas', 'Gastos en conjunto'],
