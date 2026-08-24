5d04e993206d92d6a9d11ca46ea1a0ce2f481009
Matias1661 <118617013+Matias1661@users.noreply.github.com>
2026-08-24 10:20:05 +0200

    Organizar Movimientos: agrega viaje Hells Week 2026 (10-23/08) a TRIP_WINDOWS

--- js/app.js
+++ js/app.js
@@ -231,6 +231,7 @@ const TRIP_WINDOWS = [
   { name: 'Málaga',                   start: '2026-05-01', end: '2026-05-03' },
   { name: 'Viaje a Portugal',         start: '2026-05-08', end: '2026-05-11' },
   { name: 'Viaje a Granada',          start: '2026-07-18', end: '2026-07-19' },
+  { name: 'Hells Week 2026',          start: '2026-08-10', end: '2026-08-23' },
 ];
 
 function tripSubcategory(concepto) {
